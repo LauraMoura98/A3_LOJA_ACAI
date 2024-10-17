e45dfd80981b7224a4d033003027e6fb1b103392
lauramoura98 <lauramoura06@gmail.com>
2024-10-16 21:08:44 -0300

    adding html files

---
+++ api/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Produto
+from .forms import ProdutoForm
+
+
+# CREATE
+def criar_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_produtos')
+    else:
+        form = ProdutoForm()
+    return render(request, 'criar_produto.html', {'form': form})
+
+
+# READ (Listar Produtos)
+def listar_produtos(request):
+    produtos = Produto.objects.all()
+    return render(request, 'listar_produto.html', {'produtos': produtos})
+
+
+# UPDATE
+def editar_produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_produtos')
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'editar_produto.html', {
+                'form': form, 'produto': produto
+            }
+        )
+
+
+# DELETE
+def deletar_produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == 'POST':
+        produto.delete()
+        return redirect('listar_produtos')
+    return render(request, 'deletar_produto.html', {'produto': produto})
