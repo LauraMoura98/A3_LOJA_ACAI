cb1518461ce50134460ccd72cb61a79215c81bad
lauramoura98 <lauramoura06@gmail.com>
2024-11-06 19:45:52 -0300

    allowing swagger

--- backend/loja_acai/urls.py
+++ backend/loja_acai/urls.py
@@ -16,19 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.permissions import AllowAny
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API Açaí Faseh",
-        default_version='v1',
-        description="Documentação da API",
-    ),
-    public=True,  
-    permission_classes=(AllowAny,),  
-)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api_rest.urls'), name='api_rest_urls')
