cc341d99b28679d790be143edbcb6c466131a381
AlunoFaseh2024 <972320116@ulife.com.br>
2024-12-05 13:33:19 -0300

    Adicionado algumas fotos na galeria CSS modificado para não depender completamente de script

--- frontend/static/js/navbar.js
+++ frontend/static/js/navbar.js
@@ -1,56 +1,61 @@
 function loadNavbar() {
     const navbarContainer = document.getElementById("navbar-container");
     const isMobile = window.innerWidth < 768;
+    const menuOpen = navbarContainer.querySelector(".menu")?.classList.contains("active"); // Guarda o estado do menu
 
-    // Define o HTML para a navbar de acordo com o dispositivo
+    let navbarHTML;
+
     if (isMobile) {
-        navbarContainer.innerHTML = `
+        navbarHTML = `
             <div class="navbar">
                 <img src="static/img/Logo.png" class="logo" alt="Logo">
                 <div class="hamburger" onclick="toggleMenu()">
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
+                    <div class="bar"></div>
                 </div>
                 <div class="menu" id="navbarMenu">
                     <a href="index.html">HOME</a>
                     <a href="produtos.html">FAÇA SEU PEDIDO</a>
                     <a href="contato.html">CONTATO</a>
-					<a href="sobre.html">SOBRE</a>
+                    <a href="sobre.html">SOBRE</a>
                     <div class="nav-icons">
-                        <a href="#carrinho"><img src="static/img/Carrinho.png" class="icon" alt="Carrinho"></a>
-                        <a href="conta.html"><img src="static/img/Conta.png" class="icon" alt="Conta"></a>
+                        <a href="carrinho.html"><img src="static/img/Carrinho.png" class="icon" alt="Carrinho"></a> <a href="conta.html"><img src="static/img/Conta.png" class="icon" alt="Conta"></a>
                     </div>
                 </div>
             </div>
         `;
     } else {
-        navbarContainer.innerHTML = `
+        navbarHTML = `
             <div class="navbar">
                 <img src="static/img/Logo.png" class="logo" alt="Logo">
                 <div class="nav-links">
                     <a href="index.html">HOME</a>
                     <a href="produtos.html">FAÇA SEU PEDIDO</a>
                     <a href="contato.html">CONTATO</a>
-					<a href="sobre.html">SOBRE</a>
+                    <a href="sobre.html">SOBRE</a>
                 </div>
                 <div class="nav-icons">
-                    <a href="#carrinho"><img src="static/img/Carrinho.png" class="icon" alt="Carrinho"></a>
-                    <a href="conta.html"><img src="static/img/Conta.png" class="icon" alt="Conta"></a>
+                    <a href="carrinho.html"><img src="static/img/Carrinho.png" class="icon" alt="Carrinho"></a> <a href="conta.html"><img src="static/img/Conta.png" class="icon" alt="Conta"></a>
                 </div>
             </div>
         `;
     }
+
+    navbarContainer.innerHTML = navbarHTML;
+
+
+    if (isMobile && menuOpen) {  // Reabre o menu se estava aberto antes do resize
+        document.getElementById("navbarMenu").classList.add("active");
+    }
 }
 
-// Função para alternar o menu no mobile
 function toggleMenu() {
     const menu = document.getElementById("navbarMenu");
     menu.classList.toggle("active");
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('open'); // Animação do hamburger (opcional - requer CSS)
 }
 
-// Carrega a navbar correta ao carregar a página
 window.addEventListener("load", loadNavbar);
-
-// Recarrega a navbar quando a janela é redimensionada
 window.addEventListener("resize", loadNavbar);
