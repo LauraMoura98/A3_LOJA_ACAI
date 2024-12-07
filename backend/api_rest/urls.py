e65e681f90b6b5557956b0dc7f67d50fbd82d06c
lauramoura98 <lauramoura06@gmail.com>
2024-12-07 10:42:57 -0300

    fixing tamanho_produto serializers

--- backend/api_rest/urls.py
+++ backend/api_rest/urls.py
@@ -4,7 +4,7 @@ from api_rest.views.produtos_views import produtos_geral, produtos_por_id
 from api_rest.views.acrescimos_views import acrescimos_geral, acrescimos_por_id
 from api_rest.views.tamanho_views import tamanhos_por_id, tamanhos_geral
 from api_rest.views.pedido_views import pedidos_geral, pedidos_por_id
-from api_rest.views.tamanho_produto_views import TamanhoProdutos_geral
+from api_rest.views.tamanho_produto_views import TamanhoProdutos_geral, TamanhoProdutos_delete
 from rest_framework_simplejwt.views import TokenObtainPairView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -35,6 +35,7 @@ urlpatterns = [
     path('pedidos/', pedidos_geral, name='pedidos_geral'),
     path('pedidos/<int:id>/', pedidos_por_id, name='pedidos_por_id'),
     path('tamanho-produtos/', TamanhoProdutos_geral, name='tamanho_produtos_geral'),
+    path('ProdutoTamanho/<int:produto_id>/<str:tamanho_id>/', TamanhoProdutos_geral, name='deletar_produto_tamanho'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
