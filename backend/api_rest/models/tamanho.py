a4beb632b89660abcda05f97d080d0500ea36880
lauramoura98 <lauramoura06@gmail.com>
2024-12-07 09:26:13 -0300

    fixing produtos id

--- backend/api_rest/models/tamanho.py
+++ backend/api_rest/models/tamanho.py
@@ -4,3 +4,6 @@ from django.db import models
 class Tamanho(models.Model):
     nome = models.CharField(max_length=50)
     descricao = models.TextField(blank=True)
+
+    def __str__(self):
+        return str(self.id)
